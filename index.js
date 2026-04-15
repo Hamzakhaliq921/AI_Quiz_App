108e2a46619f7eb5173f6ed8794c589be8c8fa69
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-14 23:55:24 -0600

    logic od set question

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 fetch(`${CONFIG.BASE_URL}/endpoint?apiKey=${CONFIG.API_KEY}`)
   .then(res => res.json())
   .then(data => console.log(data));
+
+  let selectedQuestions = 5;
+let selectedDifficulty = "easy";
+
+function setQuestions(num){
+
+}
