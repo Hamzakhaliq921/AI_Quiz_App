451972dd5e34067e54b2b3f74daa2efe33cfed76
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-15 00:32:31 -0600

    set difficulty and set mcqs mthod added

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-fetch(`${CONFIG.BASE_URL}/endpoint?apiKey=${CONFIG.API_KEY}`)
-  .then(res => res.json())
-  .then(data => console.log(data));
+
 
   let selectedQuestions = 5;
 let selectedDifficulty = "easy";
 
 function setQuestions(num){
 
+selectedQuestions = num;
+
+document.getElementById("num").value = num;
+
+document.querySelectorAll(".mcq-buttons button").forEach(btn=>btn.classList.remove("active"));
+
+event.target.classList.add("active");
 }
+document.addEventListener('DOMContentLoaded',function(){
+  let input=document.getElementById('num')
+  input.addEventListener('input',function (){
+    const value=this.value
+    console.log(value);
+    if(value>0 && value<50)
+    {
+      selectedQuestions=value
+          document.querySelectorAll(".mcq-buttons button")
+          .forEach(btn=>btn.classList.remove("active"));
+
+    }
+    
+  });
+
+});
+function setDifficulty(format){
+selectedDifficulty=format
+document.querySelectorAll(".difficulty-buttons button")
+          .forEach(btn=>btn.classList.remove("active"));
+  event.target.classList.add('active')
+}
+
